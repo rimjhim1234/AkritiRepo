811f4aded788cbb133e7a7b93dd2fa93e07567a2
aakriti <aakriti18899@gmail.com>
2023-04-04 17:24:57 +0530

    first commit

---
+++ GitInfo/src/test/java/com/practice/git/GitInfo/GitInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.git.GitInfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
